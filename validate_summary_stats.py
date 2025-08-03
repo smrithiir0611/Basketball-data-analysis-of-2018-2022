119f6c1ff2ac9ed4266c934a6442f913dc073592
smrithiir0611 <smradhak@syr.edu>
2025-08-03 16:22:57 -0400

    Update validate_summary_stats.py

--- validate_summary_stats.py
+++ validate_summary_stats.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-# Load your dataset
+# Loading the dataset
 df = pd.read_csv('teamstats.csv')
 
-# Select key numeric columns to validate
+# To select key numeric columns to validate
 cols_to_check = ['w', 'ppg', 'ast', 'gp', 'tov', 'pf', 'l']
 
-# Generate summary stats (count, mean, std, min, 25%, 50%, 75%, max)
+# To generate summary stats (count, mean, std, min, 25%, 50%, 75%, max)
 summary_stats = df[cols_to_check].describe()
 
 print("Summary Statistics:\n", summary_stats)
