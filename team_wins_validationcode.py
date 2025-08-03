4e95e7907baee55c5a0e8b4e8e241e52f1154c61
smrithiir0611 <smradhak@syr.edu>
2025-08-03 16:19:27 -0400

    Update team_wins_validationcode.py

--- team_wins_validationcode.py
+++ team_wins_validationcode.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-# Load dataset
+# Loading of the dataset
 df = pd.read_csv('teamstats.csv')
 
-# Calculate total wins per team and sort descending
+# To calculate total wins per team and sort descending
 team_wins = df.groupby('team')['w'].sum().sort_values(ascending=False)
 
 print("Total wins per team:")
