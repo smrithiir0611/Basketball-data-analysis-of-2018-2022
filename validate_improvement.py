3117c0dfe2826d55b5385524def78760e34c0290
smrithiir0611 <smradhak@syr.edu>
2025-08-03 16:21:53 -0400

    Update validate_improvement.py

--- validate_improvement.py
+++ validate_improvement.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
+# Here we are loading the dataset
 df = pd.read_csv('teamstats.csv')
+
 df_sorted = df.sort_values(['team', 'season'])
 
-# Calculate year-over-year win improvement
+# To calculate year-over-year win improvement
 df_sorted['win_diff'] = df_sorted.groupby('team')['w'].diff()
 
-# Average improvement per team
+# To find out the average improvement per team
 team_improvement = df_sorted.groupby('team')['win_diff'].mean().sort_values(ascending=False)
 
 print("Top teams by average yearly improvement in wins:")
